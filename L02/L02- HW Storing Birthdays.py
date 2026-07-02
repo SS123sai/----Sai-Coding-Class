c736c293452c10b0030b948633a3f48741ef26a2
SS123sai <bindu.manoj3@gmail.com>
2026-07-02 11:55:00 +0400

    L02

---
+++ L02/L02- HW Storing Birthdays.py
@@ -0,0 +1,21 @@
+birthday1= input("Weh is your birthday, Anshika? ")
+print()
+
+birthday2= input("When is your birthday, Jessica? ")
+print()
+
+birthday3= input("Wehn is your birthday Sai? ")
+print()
+
+birthday4= input("When is your birthday, Kathryn? ")
+print()
+
+birthday5= input("When is your birthday, Jack? ")
+print()
+
+print("Anshika's birthday is " +birthday1)
+print("Jessica's birthday is " +birthday2)
+print("Sai's birthday is " +birthday3)
+print("Kathryn's birthday is " +birthday4)
+print("Jack's birthday is " +birthday5)
+
