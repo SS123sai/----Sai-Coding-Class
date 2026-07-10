48cf73672c7835eeef1aa24922d3c03069fe46a8
SS123sai <bindu.manoj3@gmail.com>
2026-07-10 13:35:17 +0400

    L03 Data types

---
+++ L03/L03- String Operation.py
@@ -0,0 +1,20 @@
+#input a word
+text= input("Enter a string: ")
+
+#Reverse String
+#using step value as -1 to iterate in reverse
+revText = text[::-1]
+text = revText
+
+print("Reverse of Given String is:")
+print(text)
+
+print()
+
+#covert to uppercase
+message= "Python Is Fun"
+upper= message.upper()
+print(upper)
+
+lower= message.lower()
+print(lower)
